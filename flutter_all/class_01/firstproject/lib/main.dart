6f493b5a8cc8a004462a1ac6afa6007e4e03fbec
rizwanahmed1981 <109456571+rizwanahmed1981@users.noreply.github.com>
2025-01-27 04:18:30 +0500

    flutter practice

--- flutter_all/class_01/firstproject/lib/main.dart
+++ flutter_all/class_01/firstproject/lib/main.dart
@@ -3,11 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Text("data")
+    );
   }
 }
